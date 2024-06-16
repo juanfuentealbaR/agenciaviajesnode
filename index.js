72229b24a63b71f85499f9b10fde99b79bedefe3
Dhereck1 <82067086+Dhereck1@users.noreply.github.com>
2024-06-16 13:36:21 -0400

    Revert "moved index.js to /api"
    This reverts commit 19f2348fd2e8aa5ed1fbdc0bd02e07d2b6edec45.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import router from '../routes/index.js';
-import db from '../config/db.js';
+import router from './routes/index.js';
+import db from './config/db.js';
 
 const app = express();
 
